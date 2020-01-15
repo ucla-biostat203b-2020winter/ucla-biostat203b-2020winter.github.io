cafb7a178b614b9797e7f060c61b988da439e447
Hua Zhou <huazhou@ucla.edu>
2020-01-15 01:00:09 +0000

    set default argument wait = FALSE

--- slides/02-linux/autoSim.R
+++ slides/02-linux/autoSim.R
@@ -4,7 +4,7 @@ nVals <- seq(100, 1000, by=100)
 for (n in nVals) {
   oFile <- paste("n", n, ".txt", sep="")
   sysCall <- paste("nohup Rscript runSim.R n=", n, " > ", oFile, sep="")
-  system(sysCall)
+  system(sysCall, wait = FALSE)
   print(paste("sysCall=", sysCall, sep=""))
 }
 
